4a0f0291956ba3bf9dbbcf06a1c07cf665d5e4da
Rex-9enrile <rexrecto422@gmail.com>
2025-09-29 14:26:09 +0800

    Add files via upload

---
+++ activity19.py
@@ -0,0 +1,16 @@
+for i in range(1, 11, 1):
+    for a in range(10, i, -1):
+        print(" ", end = " ")
+    for b in range(1, i, 1):
+        print("*", end = " ")
+    for c in range(1, i, 1):
+        print("*", end = " ")
+    print()
+for a in range(1, 11, 1):
+    for b in range(1, a, 1):
+        print(" ", end = " ")
+    for c in range(10, a, -1):
+        print("*", end = " ")
+    for d in range(10, a, -1):
+        print("*", end = " ")
+    print()
